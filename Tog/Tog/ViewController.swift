e9fdc766d8a069882bb0b160e4d8986454bff6f9
lijingfu <lijingfu@zsxq.com>
2022-02-07 15:49:35 +0800

    Init

---
+++ Tog/Tog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tog
+//
+//  Created by Jingfu Li on 2022/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
